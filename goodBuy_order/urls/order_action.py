eacb03d3309ab062ad0f15640bf85335f0142120
11056015 <11056015@ntub.edu.tw>
2025-08-25 00:42:38 +0800

    接單頁css order_action有註解掉一個東西

--- goodBuy_order/urls/order_action.py
+++ goodBuy_order/urls/order_action.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('checkout/address_payment', checkout_step2, name='checkout_address_payment'),
     path('buyer_action/<int:order_id>/', buyer_action, name='buyer_action'),
     path('seller_action/<int:order_id>/', seller_action, name='seller_action'),
-    path('batch_seller_action/', batch_seller_action, name='batch_seller_action'),
+    #path('batch_seller_action/', batch_seller_action, name='batch_seller_action'),
 ]
