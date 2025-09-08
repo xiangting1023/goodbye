b151e42fb848e1c3331858148a2314d6023065e1
11056012 <05162006keke@gmail.com>
2025-09-08 23:17:19 +0800

    comment model改成同時兼容買賣家雙向評價

--- goodBuy_order/models/comment.py
+++ goodBuy_order/models/comment.py
@@ -6,16 +6,18 @@ from .order import Order
 # 訂單評價&留言
 # -------------------------
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE) 
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # 評論的發表者
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_received")  # 評論的對象
+    role = models.CharField(max_length=10, choices=[("buyer", "Buyer"), ("seller", "Seller")])  # 發表者角色
     rank = models.IntegerField()
     comment = models.TextField()
     update = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['user', 'order'], name='unique_user_order_comment')
         ]
-    
+
     def __str__(self):
-        return f'{self.rank}{self.comment}'
+        return f'{self.role}: {self.rank} {self.comment}'
